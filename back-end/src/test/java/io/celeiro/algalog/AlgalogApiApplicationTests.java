afd1080f057887719df4afaa6ba206345f725b89
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-05-31 19:56:59 -0300

    Initial project

---
+++ back-end/src/test/java/io/celeiro/algalog/AlgalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.celeiro.algalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
