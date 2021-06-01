a0fe580eb838c4fdcdab95345561e831dc1dfe9d
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-05-31 21:44:46 -0300

    Method Mapping

--- back-end/src/main/java/io/celeiro/algalog/controller/ClientController.java
+++ back-end/src/main/java/io/celeiro/algalog/controller/ClientController.java
@@ -2,7 +2,19 @@ package io.celeiro.algalog.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.celeiro.algalog.domain.model.Client;
@@ -11,13 +23,55 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/clients")
 public class ClientController {
 	
 	//@Autowired
 	private ClientRepository clientRepository;
 
-	@GetMapping("/clients")
-	public List<Client> listar() {
+	@GetMapping
+	public List<Client> listAllClients() {
 		return clientRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> listClient(@PathVariable Long id) {
+		
+		//Optional
+		return clientRepository.findById(id)
+				//.map(client -> ResponseEntity.ok(client))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@Transactional
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Client addClient(@Valid @RequestBody Client client) {
+		
+		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Client> updateClient(@PathVariable Long id, @Valid @RequestBody Client client) {
+		
+		if(!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(id);
+		client = clientRepository.save(client);
+		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+		
+		if(!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- back-end/src/main/java/io/celeiro/algalog/domain/repository/ClientRepository.java
+++ back-end/src/main/java/io/celeiro/algalog/domain/repository/ClientRepository.java
@@ -1,5 +1,7 @@
 package io.celeiro.algalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import io.celeiro.algalog.domain.model.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
+	
+	List<Client> findByName(String name);
+	List<Client> findByNameContaining(String name);
 }
